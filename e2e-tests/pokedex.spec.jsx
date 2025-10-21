9b8ae0f85e322c0fe65830670ea6e03588aa3289
Kivistö Kasper <kivisto.kasper@protonmail.com>
2025-10-21 16:18:28 +0300

    ex 11.9 - new test works locally

--- e2e-tests/pokedex.spec.jsx
+++ e2e-tests/pokedex.spec.jsx
@@ -8,4 +8,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('navigation works', async ({ page }) => {
+    await page.goto('')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+
+  })
 })
